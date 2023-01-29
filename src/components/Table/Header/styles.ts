12f5e2e4216bdfa1d715471894aecfc02804ac6b
k3v1n <kevinjhosepct@gmail.com>
2023-01-28 19:54:52 -0500

    Added headerTable styles with props

---
+++ src/components/Table/Header/styles.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+import { PropsHeader } from ".";
+
+export const HeaderStyle = styled.th<PropsHeader>`
+    background-color: ${props => props.bgColor};
+    color: ${props => props.color};
+    width: ${props => props.width};
+`;
