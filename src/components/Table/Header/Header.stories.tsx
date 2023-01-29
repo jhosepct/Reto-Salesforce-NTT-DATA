4a50a75f4d5f3ceeae8429570a95a9c1268447cd
k3v1n <kevinjhosepct@gmail.com>
2023-01-28 21:13:37 -0500

    Added stories and documentation

--- src/components/Table/Header/Header.stories.tsx
+++ src/components/Table/Header/Header.stories.tsx
@@ -1,6 +1,38 @@
-import THeader from './index'
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { THeader } from '.';
+
 
 export default {
-    title: 'Table Header',
+    title: 'Table/Header',
     component: THeader,
-}
+    argTypes: {
+        bgColor: { control: 'color' },
+        color: { control: 'color' },
+        align: { control: { type: 'select', options: ['left', 'center', 'right'] } }
+    },
+} as ComponentMeta<typeof THeader>;
+
+const Template: ComponentStory<typeof THeader> = (args) => <THeader {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+    children: 'Text',
+    width: '200px',
+};
+
+export const Rigth = Template.bind({});
+
+Rigth.args = {
+    width: '200px',
+    children: 'Text',
+    align: 'right'
+};
+
+export const Left = Template.bind({});
+
+Left.args = {
+    width: '200px',
+    children: 'Text',
+    align: 'left'
+};
