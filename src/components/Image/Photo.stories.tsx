bf07b9b9e14cfb8d9db97b00a19a79ad1aa00e0a
k3v1n <kevinjhosepct@gmail.com>
2023-01-29 11:37:24 -0500

    Added structure component

---
+++ src/components/Image/Photo.stories.tsx
@@ -0,0 +1,12 @@
+import { Photo } from ".";
+
+export default {
+    title: 'Components/Image',
+    component: Photo,
+}
+
+export const Default = () => {
+    return (
+        <Photo />
+    )
+}
---
+++ src/components/Image/index.tsx
@@ -0,0 +1,9 @@
+import { ImageStyle } from "./styles"
+
+export const Photo = () => {
+    return (
+        <ImageStyle>
+            <img src="" />
+        </ImageStyle>
+    )
+}
