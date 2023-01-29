845a188e66563247cc98b1f7df16245f44211358
k3v1n <kevinjhosepct@gmail.com>
2023-01-29 00:55:11 -0500

    Get data with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const App = () => {
+  const [data, setData] = useState([]);
+  const baseUrl = "https://randomuser.me/api/?";
+  useEffect(() => {
+    axios.get(`${baseUrl}results=10`).then((res) => {
+      setData(res.data.results);
+    });
+  }, []);
   return (
     <div>App</div>
   )
