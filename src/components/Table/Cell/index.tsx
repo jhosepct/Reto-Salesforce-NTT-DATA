b3460b82bc900aafbf871cef2b803f14e0906387
k3v1n <kevinjhosepct@gmail.com>
2023-01-29 12:41:45 -0500

    Added type photo and default cell

--- src/components/Table/Cell/index.tsx
+++ src/components/Table/Cell/index.tsx
@@ -1,9 +1,11 @@
 import { PropsCell } from "../../../utils/interfaces/components";
-import { CellStyle } from "./styles";
+import { Photo } from "../../Image";
+import { CellStyle, StyleContainer } from "./styles";
 
 export const TCell = ({
   // Default values
   children,
+  type,
   width = 'auto',
   bgColor = '#b4c6e7',
   color = '#000000',
@@ -13,6 +15,17 @@ export const TCell = ({
 
   // Props for HeaderStyle
   const propsStyle = { width: width, bgColor: bgColor, color: color, align: align }
+  const url = children?.toString();
+
+  if (type === 'photo') {
+    return (
+      <CellStyle {...propsStyle} headers={headers}>
+        <StyleContainer>
+          <Photo src={url} />
+        </StyleContainer>
+      </CellStyle>
+    )
+  }
 
   return <CellStyle {...propsStyle} headers={headers}>{children}</CellStyle>
 };
