30fba2062213e7bd03e4a547dcd7e28798716a80
k3v1n <kevinjhosepct@gmail.com>
2023-01-29 11:57:20 -0500

    Added styles of wrapper and img

--- src/components/Image/styles.ts
+++ src/components/Image/styles.ts
@@ -1,3 +1,16 @@
 import styled from "@emotion/styled";
 
-export const ImageStyle = styled.div``;
+export const ImageStyle = styled.div`
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    overflow: hidden;
+    background: #333944;
+
+    // If the image is a child of the Image component
+    & img {
+        width: 100%;
+        height: auto;
+        color: #fff;
+    }
+`;
