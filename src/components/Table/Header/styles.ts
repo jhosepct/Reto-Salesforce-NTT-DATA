80f60dbd074d68e1b0e92e206cfdb8c6f32a0ac0
k3v1n <kevinjhosepct@gmail.com>
2023-01-28 21:12:04 -0500

    Added text-align and padding

--- src/components/Table/Header/styles.ts
+++ src/components/Table/Header/styles.ts
@@ -5,4 +5,6 @@ export const HeaderStyle = styled.th<PropsHeader>`
     background-color: ${props => props.bgColor};
     color: ${props => props.color};
     width: ${props => props.width};
+    text-align: ${props => props.align};
+    padding: 5px;
 `;
