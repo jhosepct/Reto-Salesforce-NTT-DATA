0fb21a3be63ffabc95786ed8180cf6a5151766d4
k3v1n <kevinjhosepct@gmail.com>
2023-01-29 13:56:40 -0500

    Added Interface to Button

--- src/utils/interfaces/components.ts
+++ src/utils/interfaces/components.ts
@@ -60,10 +60,35 @@ export interface PropsHeader {
 
 }
 
+// Props for the Image component
 export interface PropsImage {
     /**
      * The image source
      *  
     */
     src?: string;
+}
+
+// Props for the Button component
+export interface ButtonProps {
+    /**
+     * Is this the principal call to action on the page?
+     */
+    primary?: boolean;
+    /**
+     * What background color to use
+     */
+    backgroundColor?: string;
+    /**
+     * How large should the button be?
+     */
+    size?: 'small' | 'medium' | 'large';
+    /**
+     * Button contents
+     */
+    label: string;
+    /**
+     * Optional click handler
+     */
+    onClick?: () => void;
 }
