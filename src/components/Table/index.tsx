8cd5b1d02c45d92d6ac3fa39feb856db608b1175
k3v1n <kevinjhosepct@gmail.com>
2023-01-29 11:06:56 -0500

    Refactor data fake with data props

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,96 +1,15 @@
+import { DataBody, DataHeader } from "../../App";
 import { TCell } from "./Cell";
 import { THeader } from "./Header";
 import TRow from "./Row";
 import { TableStyle } from "./style";
 
-interface DataHeader {
-    name: string;
-    width: string;
-    align?: 'left' | 'center' | 'right';
-    id: string;
+interface TableProps {
+    dataHeader: DataHeader[], dataBody: DataBody[]
 }
 
-const dataHeader: DataHeader[] = [
-    {
-        name: 'Nombre',
-        width: '200px',
-        align: 'left',
-        id: 'name'
 
-    },
-    {
-        name: 'Apellido',
-        width: '200px',
-        align: 'left',
-        id: 'lastname'
-    },
-    {
-        name: 'Edad',
-        width: '200px',
-        id: 'age'
-    },
-    {
-        name: 'Genero',
-        width: '200px',
-        id: 'gender'
-    },
-    {
-        name: 'Email',
-        width: '600px',
-        align: 'left',
-        id: 'email'
-    },
-    {
-        name: 'Nacionalidad',
-        width: '200px',
-        id: 'nationality'
-    },
-    {
-        name: 'Foto',
-        width: '200px',
-        id: 'photo'
-    }
-]
-
-const getKeyValue = <U extends keyof T, T extends object>(key: U) => (obj: T) =>
-    obj[key];
-
-interface DataBody {
-    name: string;
-    lastname: string;
-    age: string;
-    gender: string;
-    email: string;
-    nationality: string;
-    photo: string;
-}
-
-const dataBody: DataBody[] = [
-    {
-        name: 'Elsie',
-        lastname: 'Fields',
-        age: '27',
-        gender: 'F',
-        email: 'elsie.fields@example.com',
-        nationality: 'US',
-        photo: ''
-    },
-    {
-        name: 'Elsie 2',
-        lastname: 'Fields 2',
-        age: '27',
-        gender: 'F',
-        email: 'elsie.fields@example.com',
-        nationality: 'US',
-        photo: ''
-    }
-]
-
-const keyOf = (key: string) => {
-    return key as keyof DataBody;
-}
-
-const Table = () => {
+const Table = ({ dataHeader, dataBody }: TableProps) => {
     return (
         <TableStyle>
             <thead>
@@ -107,7 +26,7 @@ const Table = () => {
                     <TRow>
                         {dataHeader.map((column) => (
                             <TCell key={column.id} align={column.align} headers={column.id} bgColor={(index + 1) % 2 == 0 ? '#d9e1f2' : '#b4c6e7'}>
-                                {row[keyOf(column.id)]}
+                                {row[column.id]}
                             </TCell>
                         ))}
 
