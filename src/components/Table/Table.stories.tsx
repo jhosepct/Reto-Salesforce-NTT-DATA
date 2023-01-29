f4dbe5f0f6bfb9cfd67131c198dd3977ae4a3b43
k3v1n <kevinjhosepct@gmail.com>
2023-01-29 11:07:58 -0500

    Added data fake of stories

--- src/components/Table/Table.stories.tsx
+++ src/components/Table/Table.stories.tsx
@@ -1,3 +1,4 @@
+import { DataBody } from '../../App';
 import Table from './index'
 
 export default {
@@ -5,9 +6,88 @@ export default {
     component: Table,
 }
 
+interface DataHeader {
+    name: string;
+    width: string;
+    align?: 'left' | 'center' | 'right';
+    id: string;
+}
+
+const dataHeader: DataHeader[] = [
+    {
+        name: 'Nombre',
+        width: '200px',
+        align: 'left',
+        id: 'name'
+
+    },
+    {
+        name: 'Apellido',
+        width: '200px',
+        align: 'left',
+        id: 'lastname'
+    },
+    {
+        name: 'Edad',
+        width: '200px',
+        id: 'age'
+    },
+    {
+        name: 'Genero',
+        width: '200px',
+        id: 'gender'
+    },
+    {
+        name: 'Email',
+        width: '600px',
+        align: 'left',
+        id: 'email'
+    },
+    {
+        name: 'Nacionalidad',
+        width: '200px',
+        id: 'nationality'
+    },
+    {
+        name: 'Foto',
+        width: '200px',
+        id: 'photo'
+    }
+]
+
+const getKeyValue = <U extends keyof T, T extends object>(key: U) => (obj: T) =>
+    obj[key];
+
+
+const dataBody: DataBody[] = [
+    {
+        name: 'Elsie',
+        lastname: 'Fields',
+        age: 27,
+        gender: 'F',
+        email: 'elsie.fields@example.com',
+        nationality: 'US',
+        photo: ''
+    },
+    {
+        name: 'Elsie 2',
+        lastname: 'Fields 2',
+        age: 27,
+        gender: 'F',
+        email: 'elsie.fields@example.com',
+        nationality: 'US',
+        photo: ''
+    }
+]
+
+const keyOf = (key: string) => {
+    return key as keyof DataBody;
+}
+
+
 export const Primary = () => {
     return (
-        <Table />
+        <Table dataHeader={dataHeader} dataBody={dataBody} />
     )
 }
 
