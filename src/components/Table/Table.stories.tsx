0c51c0c408f1f41b46183fe55ab207b73632f035
k3v1n <kevinjhosepct@gmail.com>
2023-01-28 19:45:10 -0500

    Created table stories

---
+++ src/components/Table/Table.stories.tsx
@@ -0,0 +1,14 @@
+import Table from './index'
+
+export default {
+    title: 'Table',
+    component: Table,
+}
+
+export const Primary = () => {
+    return (
+        <Table />
+    )
+}
+
+Primary.storyName = 'Default';
