eb8093043b4fa51a0c99cfec33fc613b3a6410e3
k3v1n <kevinjhosepct@gmail.com>
2023-01-28 23:44:38 -0500

    Modified tag

--- src/components/Table/Cell/styles.ts
+++ src/components/Table/Cell/styles.ts
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 import { PropsCell } from "../../../utils/interfaces/components";
 
-export const CellStyle = styled.th<PropsCell>`
+export const CellStyle = styled.td<PropsCell>`
     background-color: ${props => props.bgColor};
     color: ${props => props.color};
     width: ${props => props.width};
