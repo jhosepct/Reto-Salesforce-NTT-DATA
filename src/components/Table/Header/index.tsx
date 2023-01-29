bf67da39d79f98b9078e470707eed6986d5ab936
k3v1n <kevinjhosepct@gmail.com>
2023-01-28 19:55:48 -0500

    Added types props and default props

--- src/components/Table/Header/index.tsx
+++ src/components/Table/Header/index.tsx
@@ -1,9 +1,27 @@
-type PropsHeader = {
+import { HeaderStyle } from "./styles"
+
+// Props for Header
+export type PropsHeader = {
     children: string | JSX.Element | JSX.Element[]
+    width: string
+    bgColor: string
+    color: string
 }
 
-const THeader = ({ children }: PropsHeader) => {
-    return <th>{children}</th>
+const THeader = ({ children, width, bgColor, color }: PropsHeader) => {
+
+    const props = { width: width, bgColor: bgColor, color: color }
+
+    return <HeaderStyle {...props}>{children}</HeaderStyle>
 }
+
+
+// Default props
+THeader.defaultProps = {
+    width: 'auto',
+    bgColor: '#4472c4',
+    color: '#ffffff'
+}
+
 
 export default THeader
