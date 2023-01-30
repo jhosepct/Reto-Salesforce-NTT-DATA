3e1a22e58c9ac7103cfc3582926f2f438bae9dea
k3v1n <kevinjhosepct@gmail.com>
2023-01-30 14:57:35 -0500

    Modified styles with state for SortType

--- src/components/Table/Header/styles.ts
+++ src/components/Table/Header/styles.ts
@@ -1,14 +1,20 @@
 import styled from "@emotion/styled";
 import { PropsHeader } from "../../../utils/interfaces/components";
+import { typography } from "../../../assets/typography";
+
+type PropsMenu = {
+    menu: boolean;
+}
 
 export const HeaderStyle = styled.th<PropsHeader>`
+    ${typography.disabled.textSelection}
+
     background-color: ${props => props.bgColor};
     color: ${props => props.color};
     width: ${props => props.width};
     text-align: ${props => props.align};
     padding: 5px;
     position: relative;
-    
     z-index: 100;
 
     ${({ sortType = '', ...props }) => props.sortable && sortable(sortType)}
@@ -17,12 +23,14 @@ export const HeaderStyle = styled.th<PropsHeader>`
 const sortable = (type: string) => {
     return `
     cursor: pointer;
-    &::after{
-        position: absolute;
+    div .fisrt-child{
+        display: flex;
+        align-items: center;
+    }
+    div .fisrt-child svg {   
+        width: 20px;
+        height: 20px;
         ${sortType(type)}
-        display: inline-block;
-
-        background-color: red;
     }
     `
 }
@@ -31,17 +39,15 @@ const sortType = (type: string) => {
     switch (type) {
         case 'asc':
             return `
-                content: '^';
-           
+                rotate: 180deg;
             `
         case 'desc':
             return `
-                content: 'v';
-           
+                rotate: 0deg;
             `
         default:
             return `
-                content: '';
+                opacity: 0.5;
             
             `
     }
@@ -51,15 +57,20 @@ export const Wrapper = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    .fisrt-child{
+        flex-grow: 1;
+    }
 `;
 
-export const MenuIcon = styled.div`
+export const MenuIcon = styled.div<PropsMenu>`
     display: flex;
     justify-content: center;
     align-items: center;
     border-radius: 50%;
     width: 20px;
     height: 20px;
+
+    ${props => `background: ${props.menu ? 'rgba(0, 0, 0, 0.09)' : ''};`}
 
     &:hover{
         background-color: rgba(0, 0, 0, 0.09);
@@ -68,6 +79,7 @@ export const MenuIcon = styled.div`
         font-size: 16px;
     }
     & div{
+        ${props => `display: ${props.menu ? 'block' : 'none'};`}
         position: absolute;
         border-radius: 5px;
         width: 139px;
@@ -81,8 +93,18 @@ export const MenuIcon = styled.div`
         list-style: none;
         display: flex;
         flex-direction: column;
-        padding: 10px 5px;
-        gap: 5px;
         align-items: center;
+    }
+    & li{
+        width: 100%;
+        cursor: pointer;
+        padding: 10px 5px;
+        text-align: center;
+        &:hover{
+            background-color: rgba(0, 0, 0, 0.09);
+        }
+    }
+    & li.active{
+        opacity: 0.5;
     }
 `;
