15ae252812dbbcbb4f90ad11af7e7cfb1f614475
k3v1n <kevinjhosepct@gmail.com>
2023-01-30 14:59:31 -0500

    Added styling for texSelection disabled

--- src/assets/typography/index.ts
+++ src/assets/typography/index.ts
@@ -2,3 +2,13 @@ export const fonts = {
     primary: `'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif`,
     secondary: `'Inter', sans-serif`,
 };
+
+export const typography = {
+    disabled: {
+        textSelection: `-youbkit-touch-callout: none; /*iOS Safari*/
+        -youbkit-user-select: none;   /* Chrome 6.0+, Safari 3.1+, Edge y Opera 15+ */
+        -moz-user-select: none;    /*Firefox*/
+        -ms-user-select: none;        /* IE 10+ y Edge */
+        user-select: none;`,
+    }
+}
