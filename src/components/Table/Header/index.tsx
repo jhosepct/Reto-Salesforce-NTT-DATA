2e10d2bfa6d350995566138730387a2ba72b438f
k3v1n <kevinjhosepct@gmail.com>
2023-01-30 14:58:07 -0500

    Added logic for sort

--- src/components/Table/Header/index.tsx
+++ src/components/Table/Header/index.tsx
@@ -3,6 +3,9 @@ import { PropsHeader } from "../../../utils/interfaces/components";
 import { HeaderStyle, MenuIcon, Wrapper } from "./styles"
 
 import { BsThreeDotsVertical } from 'react-icons/bs';
+import { AiOutlineCaretDown } from 'react-icons/ai';
+
+import { DataBody } from '../../../App'
 
 export const THeader = ({
     // Default values
@@ -12,39 +15,53 @@ export const THeader = ({
     color = '#ffffff',
     align = 'center',
     sortable = false,
-    sortType = ''
+    sortType = '',
+    handleClickSort,
+    type
 }: PropsHeader) => {
     const [sort, setSort] = useState<'asc' | 'desc' | ''>(sortType);
 
+    const [menu, setMenu] = useState<boolean>(false);
+
+    const propMenu = { menu: menu }
+
     const handleClick = () => {
-        if (sort === '') {
-            setSort('asc')
+        if (sort === '' || sort === 'desc') {
+            setSort('asc');
+            handleClickSort?.('asc', type);
         } else {
-            if (sort === 'asc') {
-                setSort('desc')
-            } else {
-                setSort('asc')
-            }
+            setSort('desc')
+            handleClickSort?.('desc', type);
         }
     }
+
+
+    const handleSort = (typeSort: 'asc' | 'desc' | '') => {
+        setSort(typeSort);
+        setMenu(false);
+        handleClickSort?.(typeSort, type);
+    }
+
+    console.log()
 
     // Props for HeaderStyle
     const props = { width: width, bgColor: bgColor, color: color, align: align, sortable: sortable, sortType: sort }
     if (sortable) {
-        return <HeaderStyle {...props} onClick={handleClick} >
-            <Wrapper>
-                <div>
-                    {children}
+        return <HeaderStyle {...props}  >
+            <Wrapper >
+                <div className="fisrt-child" onClick={handleClick}>
+                    <p>
+                        {children}
+                    </p>
+                    <AiOutlineCaretDown />
                 </div>
-                <MenuIcon onClick={() => {
-                    console.log('click')
-                }}>
+                <MenuIcon {...propMenu} onClick={() => setMenu(!menu)}>
                     <BsThreeDotsVertical />
                     <div>
                         <ul>
-                            <li>Sort by ASC</li>
-                            <li>Sort by DESC</li>
-                            <li>Unsort</li>
+                            <li className={sort === "asc" ? "active" : ""} onClick={() => handleSort('asc')}>Sort by ASC</li>
+                            <li className={sort === "desc" ? "active" : ""} onClick={() => handleSort('desc')}>Sort by DESC</li>
+                            <li className={sort === "" ? "active" : ""} onClick={() => handleSort('')}>Unsort</li>
                         </ul>
                     </div>
                 </MenuIcon>
