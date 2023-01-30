acb346ad232558d80242c4a65918b11f94f474d7
k3v1n <kevinjhosepct@gmail.com>
2023-01-29 23:54:39 -0500

    modifying to sort

--- src/components/Table/Header/index.tsx
+++ src/components/Table/Header/index.tsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import { PropsHeader } from "../../../utils/interfaces/components";
-import { HeaderStyle } from "./styles"
+import { HeaderStyle, MenuIcon, Wrapper } from "./styles"
+
+import { BsThreeDotsVertical } from 'react-icons/bs';
 
 export const THeader = ({
     // Default values
@@ -7,11 +10,46 @@ export const THeader = ({
     width = 'auto',
     bgColor = '#4472c4',
     color = '#ffffff',
-    align = 'center'
+    align = 'center',
+    sortable = false,
+    sortType = ''
 }: PropsHeader) => {
+    const [sort, setSort] = useState<'asc' | 'desc' | ''>(sortType);
+
+    const handleClick = () => {
+        if (sort === '') {
+            setSort('asc')
+        } else {
+            if (sort === 'asc') {
+                setSort('desc')
+            } else {
+                setSort('asc')
+            }
+        }
+    }
 
     // Props for HeaderStyle
-    const props = { width: width, bgColor: bgColor, color: color, align: align }
-
-    return <HeaderStyle {...props}>{children}</HeaderStyle>
+    const props = { width: width, bgColor: bgColor, color: color, align: align, sortable: sortable, sortType: sort }
+    if (sortable) {
+        return <HeaderStyle {...props} onClick={handleClick} >
+            <Wrapper>
+                <div>
+                    {children}
+                </div>
+                <MenuIcon onClick={() => {
+                    console.log('click')
+                }}>
+                    <BsThreeDotsVertical />
+                    <div>
+                        <ul>
+                            <li>Sort by ASC</li>
+                            <li>Sort by DESC</li>
+                            <li>Unsort</li>
+                        </ul>
+                    </div>
+                </MenuIcon>
+            </Wrapper>
+        </HeaderStyle>
+    }
+    return <HeaderStyle {...props} >{children}</HeaderStyle>
 };
