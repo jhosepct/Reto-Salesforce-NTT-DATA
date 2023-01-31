6a2f99b7ca6286f8036308922a840b65f349d492
k3v1n <kevinjhosepct@gmail.com>
2023-01-31 17:11:48 -0500

    Add props style width

--- src/components/Image/styles.ts
+++ src/components/Image/styles.ts
@@ -1,8 +1,9 @@
 import styled from "@emotion/styled";
+import { PropsImage } from "../../utils/interfaces/components";
 
-export const ImageStyle = styled.div`
-    width: 70px;
-    height: 70px;
+export const ImageStyle = styled.div<PropsImage>`
+    width: ${props => props.width || '72px'};
+    height: ${props => props.width || '72px'};
     border-radius: 50%;
     overflow: hidden;
     background: transparent;
