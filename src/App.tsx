783c47e7352cc546ed66e7c978c7032e2469eff6
k3v1n <kevinjhosepct@gmail.com>
2023-02-01 13:35:52 -0500

    Home page complete with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,9 @@
-import { useEffect, useState } from "react";
-
-import { createData, dataHeader } from "./utils/data/data";
-import Table from "./components/Table";
-
-import { CSVLink } from "react-csv";
-
-import axios from "axios";
-import { Button } from "./components/Button";
-import { DataBody } from "./utils/interfaces/data";
-
-
-
-
+import { Home } from "./pages/Home";
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const [newData, setNewData] = useState<DataBody[]>([]);
-  const [dataExport, setDataExport] = useState<DataBody[]>([]);
-  const baseUrl = "https://randomuser.me/api/?";
-  useEffect(() => {
-    axios.get(`${baseUrl}results=10`).then((res) => {
-      setData(res.data.results);
-    });
-  }, []);
-
-  useEffect(() => {
-    const tempNewData: DataBody[] = data.map((item: any) => {
-      return createData(item);;
-    });
-    setNewData(tempNewData);
-  }, [data]);
-
-  const handleCLickData = () => {
-    const tempDataExport: DataBody[] = newData.map((item: any) => {
-      return { ...item, photo: item.photo.large }
-    })
-    setDataExport(tempDataExport);
-  }
 
   return (
-    <div>
-      <CSVLink data={dataExport} separator={";"} filename="datosApi.csv">
-        <Button label="Exportara CSV" primary onClick={handleCLickData} />
-      </CSVLink>
-
-      <Table dataHeader={dataHeader} dataBody={newData} />
-    </div>
+    <Home />
   )
 }
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,49 @@
 import styled from "@emotion/styled";
+import { typography } from "../../assets/typography";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+`;
+
+export const WrapperTable = styled.div`
+    max-height: 450px;
+    overflow-x: auto;
+    scrollbar-width: thin;    
+    thead{
+        position: sticky;
+        top: 0;
+        display: table-header-group;
+    }
+    
+    // Scrollbar design
+
+    &::-webkit-scrollbar {
+        -webkit-appearance: none;
+    }
+    
+    &::-webkit-scrollbar:vertical {
+        width:10px;
+    }
+    
+    &::-webkit-scrollbar-button:increment,&::-webkit-scrollbar-button {
+        display: none;
+    } 
+    
+    &::-webkit-scrollbar:horizontal {
+        height: 10px;
+    }
+    
+    &::-webkit-scrollbar-thumb {
+        background-color: #797979;
+        border-radius: 20px;
+        border: 2px solid #f1f2f3;
+    }
+    
+    &::-webkit-scrollbar-track {
+        border-radius: 10px;  
+    }
+`;
