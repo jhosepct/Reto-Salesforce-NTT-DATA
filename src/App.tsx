82698d1ec36f79223a524827849696266ea40ec5
k3v1n <kevinjhosepct@gmail.com>
2023-02-01 00:28:38 -0500

    Fixed problem when exporting data

--- src/App.tsx
+++ src/App.tsx
@@ -72,6 +72,7 @@ const dataHeader: DataHeader[] = [
 const App = () => {
   const [data, setData] = useState([]);
   const [newData, setNewData] = useState<DataBody[]>([]);
+  const [dataExport, setDataExport] = useState<DataBody[]>([]);
   const baseUrl = "https://randomuser.me/api/?";
   useEffect(() => {
     axios.get(`${baseUrl}results=10`).then((res) => {
@@ -85,10 +86,18 @@ const App = () => {
     });
     setNewData(tempNewData);
   }, [data]);
+
+  const handleCLickData = () => {
+    const tempDataExport: DataBody[] = newData.map((item: any) => {
+      return { ...item, photo: item.photo.large }
+    })
+    setDataExport(tempDataExport);
+  }
+
   return (
     <div>
-      <CSVLink data={newData} separator={";"} filename="datosApi.csv">
-        <Button label="Exportara CSV" primary />
+      <CSVLink data={dataExport} separator={";"} filename="datosApi.csv">
+        <Button label="Exportara CSV" primary onClick={handleCLickData} />
       </CSVLink>
 
       <Table dataHeader={dataHeader} dataBody={newData} />
