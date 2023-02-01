9fca6a4a6dc74fec61c14bb19a07ea5af3561822
k3v1n <kevinjhosepct@gmail.com>
2023-02-01 13:37:51 -0500

    Home page complete with styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,49 @@
+import { useEffect, useState } from "react";
+
+import { Button } from "../../components/Button"
+import Table from "../../components/Table"
+
+import { DataBody } from "../../utils/interfaces/data";
+import { createData, dataHeader } from "../../utils/data/data";
+
+import axios from "axios";
+import { CSVLink } from "react-csv"
+import { Wrapper, WrapperTable } from "./styles";
+
 export const Home = () => {
+    const [data, setData] = useState([]);
+    const [newData, setNewData] = useState<DataBody[]>([]);
+    const [dataExport, setDataExport] = useState<DataBody[]>([]);
+    const baseUrl = "https://randomuser.me/api/?";
+    useEffect(() => {
+        axios.get(`${baseUrl}results=10`).then((res) => {
+            setData(res.data.results);
+        });
+    }, []);
+
+    useEffect(() => {
+        const tempNewData: DataBody[] = data.map((item: any) => {
+            return createData(item);;
+        });
+        setNewData(tempNewData);
+    }, [data]);
+
+    const handleCLickData = () => {
+        const tempDataExport: DataBody[] = newData.map((item: any) => {
+            return { ...item, photo: item.photo.large }
+        })
+        setDataExport(tempDataExport);
+    }
+
     return (
-        <div>index</div>
+        <Wrapper>
+            <h1>Reto Salesforce NTT DATA</h1>
+            <CSVLink data={dataExport} separator={";"} filename="datosApi.csv">
+                <Button label="Exportar datos a CSV" onClick={handleCLickData} />
+            </CSVLink>
+            <WrapperTable>
+                <Table dataHeader={dataHeader} dataBody={newData} />
+            </WrapperTable>
+        </Wrapper>
     )
 }
